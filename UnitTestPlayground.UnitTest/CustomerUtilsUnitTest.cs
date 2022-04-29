6a04938eb508828d62043895ec90ad037b94ea37
Onur <onur.yenigun@ucay.com.tr>
2022-04-29 09:13:03 +0300

    Initial Commit

---
+++ UnitTestPlayground.UnitTest/CustomerUtilsUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using Moq;
+using NUnit.Framework;
+using UnitTestPlayground.Models;
+using UnitTestPlayground.Service;
+
+namespace UnitTestPlayground.UnitTest;
+
+public class CustomerUtilsUnitTest
+{
+
+    // Method Name Format: UnitOfWork_Condition_ExpectedResult
+
+    [Test]
+    public void Customer_WithInvalidData_InvalidData()
+    {
+        //Arrange
+        var customerMethods = new CustomerUtils(null);
+        var customerModel = new CustomerModel();
+
+        //Action
+        var response = customerMethods.RegisterCustomer(customerModel);
+
+        //Assertion
+        Assert.AreEqual(response, RegistrationStatus.INVALIDDATA);
+    }
+
+    [Test]
+    public void Customer_WithInvalidAge_InvalidData()
+    {
+        var customerMethods = new CustomerUtils(null);
+        var customerModel = new CustomerModel() { IdentityNumber = "IdentityNumber", Email = "email@outlook.com", Age = 120 };
+
+        RegistrationStatus response = customerMethods.RegisterCustomer(customerModel);
+
+        Assert.AreEqual(response, RegistrationStatus.INVALIDDATA);
+    }
+
+    [Test]
+    public void Customer_WithValidData_Success()
+    {
+        var mockValidator = new Mock<ICustomerService>();
+        mockValidator.Setup(i => i.RegisterCustomer(It.IsAny<CustomerModel>())).Returns(1);
+        var customerMethods = new CustomerUtils(mockValidator.Object);
+        var customerModel = new CustomerModel() { IdentityNumber = "IdentityNumber", Email = "email@outlook.com", Age = 28 };
+
+        var response = customerMethods.RegisterCustomer(customerModel);
+
+        Assert.AreEqual(response, RegistrationStatus.SUCCESS);
+    }
+
+
+    [Test]
+    public void Customer_WithValidData_Failure()
+    {
+        var mockValidator = new Mock<ICustomerService>();
+        mockValidator.Setup(i => i.RegisterCustomer(It.IsAny<CustomerModel>())).Returns(-1);
+        var customerMethods = new CustomerUtils(mockValidator.Object);
+        var customerModel = new CustomerModel() { IdentityNumber = "IdentityNumber", Email = "email@outlook.com", Age = 28 };
+
+        var response = customerMethods.RegisterCustomer(customerModel);
+
+        Assert.AreEqual(response, RegistrationStatus.FAILURE);
+    }
+}
---
+++ UnitTestPlayground/CustomerUtils.cs
@@ -0,0 +1,30 @@
+﻿using UnitTestPlayground.Models;
+using UnitTestPlayground.Service;
+
+namespace UnitTestPlayground;
+public class CustomerUtils
+{
+
+    private readonly ICustomerService _customerService;
+
+    public CustomerUtils(ICustomerService customerService)
+    {
+        _customerService = customerService;
+    }
+
+    public RegistrationStatus RegisterCustomer(CustomerModel customer)
+    {
+        if (string.IsNullOrEmpty(customer.IdentityNumber) | string.IsNullOrEmpty(customer.Email))
+            return RegistrationStatus.INVALIDDATA;
+
+        if (!Enumerable.Range(8, 100).Contains(customer.Age))
+            return RegistrationStatus.INVALIDDATA;
+
+        var response = _customerService.RegisterCustomer(customer);
+        if (response <= 0)
+            return RegistrationStatus.FAILURE;
+
+        return RegistrationStatus.SUCCESS;
+    }
+}
+
